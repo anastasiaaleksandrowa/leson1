16a5485a86ee9a0765f268453af840cc01d8b3c2
Kira <kirnan_t@outlok.com>
2024-03-21 21:21:31 +0700

    problem task 4

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5;
+int b = 3;
+int c = 34;
+int d = 6;
+int c = 2;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+console.writeline(max);
